9e320039d73d766ca9951a178d77a1e87fec122d
Rownok Nowrose <rnowrose91@gmail.com>
2026-07-24 18:37:42 -0400

    fix: got rod of all the nessesary files that are not needed

--- src/plugins/supabase.ts
+++ src/plugins/supabase.ts
@@ -1,11 +1,6 @@
-import { GraphQLClient } from 'graphql-request'
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
-export const supabaseClient = new GraphQLClient(`${SUPABASE_URL}/graphql/v1`, {
-  headers: {
-    apikey: SUPABASE_ANON_KEY,
-    Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
-  },
-})
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
